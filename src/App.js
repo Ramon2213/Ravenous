669250fec47351643655f7f4480b6f7839cb73f9
Ramon2213 <ramondenboer1995@hotmail.com>
2024-09-03 12:24:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import styles from "./Apps.css";
-
+import styles from "./App.css";
 import BusinessList from "./components/BusinessList/BusinessList";
 import SearchBar from "./components/SearchBar/SearchBar";
 
 const App = () => {
   return (
     <div className={styles.App}>
-      <h1>ravenous</h1>
+      <h1>Ravenous</h1>
       <SearchBar />
       <BusinessList />
     </div>
